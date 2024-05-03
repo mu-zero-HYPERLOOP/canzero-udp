1a0031fe85956bf66fce6782c71ae3f2e79827ae
kistenklaus <karlsasssie@gmail.com>
2024-05-03 18:37:55 +0200

    major refactor (previously in can-tcp-bridge-rs)

---
+++ src/frame.rs
@@ -0,0 +1,29 @@
+use std::{net::IpAddr, time::{Duration, Instant}};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum UdpFrame {
+    Hello(HelloFrame),
+    NDF(NetworkDescriptionFrame)
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct HelloFrame {
+    pub service_name : String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct NetworkDescriptionFrame {
+    pub service_name : String,
+    pub service_port : u16,
+    pub time_since_sor : Duration,
+    pub server_name: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct NetworkDescription {
+    pub timebase : Instant,
+    pub server_name: String,
+    pub service_port : u16,
+    pub server_addr : IpAddr,
+}
